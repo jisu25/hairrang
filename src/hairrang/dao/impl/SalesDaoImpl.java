05186550dfa346f89265ef2d3226f0dc6e54e30e
kimdeahun6317 <kdh0161@naver.com>
2020-08-27 16:53:49 +0900

    공통적인 부분수정

--- src/hairrang/dao/impl/SalesDaoImpl.java
+++ src/hairrang/dao/impl/SalesDaoImpl.java
@@ -26,7 +26,10 @@ public class SalesDaoImpl implements SalesDao{
 
 	@Override
 	public List<Sales> selectSalesByAll() {
-		String sql = "SELECT * FROM SALES s JOIN HAIR h ON (s.HAIR_NO = h.HAIR_NO ) JOIN GUEST g ON (g.GUEST_NO = s.GUEST_NO) JOIN EVENT e ON (s.EVENT_NO = e.EVENT_NO)";
+		String sql = "SELECT * FROM SALES s \r\n" + 
+				"	LEFT OUTER JOIN HAIR h USING (HAIR_NO)\r\n" + 
+				"	LEFT OUTER JOIN GUEST g USING (GUEST_NO) \r\n" + 
+				"	LEFT OUTER JOIN EVENT e USING (EVENT_NO)";
 		try(Connection con = JdbcUtil.getConnection();
 					PreparedStatement pstmt = con.prepareStatement(sql);
 					ResultSet rs = pstmt.executeQuery()){
