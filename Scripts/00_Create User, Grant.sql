5c079ee390da9aa76222a4c464cfa59aba139513
jisu25 <kimpigu.zz@gmail.com>
2020-08-27 17:49:38 +0900

    sql 구문 조금

---
+++ Scripts/00_Create User, Grant.sql
@@ -0,0 +1,13 @@
+-- Hairrang 계정 생성
+CREATE USER hairrang IDENTIFIED BY rootroot;
+
+-- Hairrang 권한 부여
+GRANT CONNECT, DBA, RESOURCE TO HAIRRANG;
+
+-- 현재 접속 계정 조회
+SELECT USER
+FROM dual;
+
+
+-- 필요시 쓰세요. 헤어랑 계정 삭제 및 모든 정보 삭제.
+DROP USER HAIRRANG CASCADE;
