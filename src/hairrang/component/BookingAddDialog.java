47c36922110a02c07831c91d6d148faabe6310c3
jisu25 <kimpigu.zz@gmail.com>
2020-08-31 22:59:07 +0900

    booking 구현중

--- src/hairrang/component/BookingAddDialog.java
+++ src/hairrang/component/BookingAddDialog.java
@@ -2,21 +2,30 @@ package hairrang.component;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
 
-public class BookingAddDialog extends JDialog {
+import hairrang.dto.Booking;
+import hairrang.dto.Guest;
+import hairrang.service.BookingService;
+
+public class BookingAddDialog extends JDialog implements ActionListener {
 
 	private BookingAddInputPanel pBookAdd = new BookingAddInputPanel();
 	private JPanel pBtns;
 	private JButton okBtn;
 	private JButton cancelBtn;
+	
+	private BookingService bService;
 
 	public BookingAddDialog() {
+		bService = new BookingService();
+		
 		initComponets();
 	}
 
@@ -34,10 +43,27 @@ public class BookingAddDialog extends JDialog {
 		getContentPane().add(pBtns, BorderLayout.SOUTH);
 		
 		okBtn = new JButton("등록");
+		okBtn.addActionListener(this);
 		pBtns.add(okBtn);
 		
 		cancelBtn = new JButton("취소");
 		pBtns.add(cancelBtn);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == okBtn) {
+			okBtnActionPerformed(e);
+		}
+	}
+	
+	
+	// 등록 버튼을 눌렀을 때 패널에서 정보 가져오기
+	protected void okBtnActionPerformed(ActionEvent e) {
+		Guest newGuest = pBookAdd.getGuest();
+		
+		
+		Booking newBook = new Booking();
+//		bService.addBook();
+		
+	}
 }
